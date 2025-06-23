74dbf9cee5c14264f489808f0749de0003e7277e
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2025-06-23 15:18:05 -0500

    Add config.example.js for devs

---
+++ config.example.js
@@ -0,0 +1,8 @@
+// config.example.js - Template for other developers
+const CONFIG = {
+  GOOGLE_CLIENT_ID: "YOUR_CLIENT_ID_HERE.apps.googleusercontent.com",
+  SPREADSHEET_ID: "YOUR_SPREADSHEET_ID_HERE", 
+  SHEET_NAME: "Sheet1"
+};
+
+window.CONFIG = CONFIG;
